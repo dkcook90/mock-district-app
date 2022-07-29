46cbd669acdf220ed9a9f9bc54f1708ca739f131
Fon <dkcook90@gmail.com>
2022-07-29 10:50:31 -0600

    removed .git from client

--- server/server.js
+++ server/server.js
@@ -11,5 +11,5 @@ app.use(express.urlencoded({ extended: true }));
 
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 });
