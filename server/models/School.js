a3aa10d0968a245e15362f259e9a78ead9871c3b
Fon <dkcook90@gmail.com>
2022-07-29 12:20:58 -0600

    finished building up models

--- server/models/School.js
+++ server/models/School.js
@@ -1,14 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+class School extends Model {}
 
-User.init(
+School.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -20,38 +15,26 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    email: {
+    address: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [8],
-      },
+    principal: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    budget: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
     },
   },
   {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      },
-    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
-    modelName: 'user',
+    underscored: true,
+    modelName: 'school',
   }
 );
 
-module.exports = User;
+module.exports = Project;
