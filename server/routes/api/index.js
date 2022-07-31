66557c8e6ff6cea847f7e64508381b101b55a130
Fon <dkcook90@gmail.com>
2022-07-30 22:56:18 -0600

    User login/signup set up on the back end

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const schoolRoutes = require('./schoolRoutes')
 const teacherRoutes = require('./teacherRoutes')
 const studentRoutes = require('./studentRoutes')
+const userRoutes = require('./userRoutes')
 
 router.use('/schools', schoolRoutes)
 router.use('/teachers', teacherRoutes)
 router.use('/students', studentRoutes)
+router.use('/users', userRoutes)
 
 module.exports = router;
